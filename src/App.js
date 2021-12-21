8bac5388143d752377a96b6282aac4c19246942e
ZICCORP <frankchuka250@gmail.com>
2021-12-21 02:57:19 +0100

    completed navigaation section

--- src/App.js
+++ src/App.js
@@ -2,38 +2,50 @@
 
 function App() {
   return <>
-    <div className="bg-black text-white">
-      <header>
-        <div>
-          <div><a href="/">
+    <div className="bg-black text-white font-mono">
+      <header className="text-sm">
+        <div className="flex justify-between pt-8 pb-4">
+          <div><a className="flex justify-start ml-24" href="/">
             <svg width="30" height="32" fill="currentcolor" style={{ display: "block" }}><path d="M29.24 22.68c-.16-.39-.31-.8-.47-1.15l-.74-1.67-.03-.03c-2.2-4.8-4.55-9.68-7.04-14.48l-.1-.2c-.25-.47-.5-.99-.76-1.47-.32-.57-.63-1.18-1.14-1.76a5.3 5.3 0 00-8.2 0c-.47.58-.82 1.19-1.14 1.76-.25.52-.5 1.03-.76 1.5l-.1.2c-2.45 4.8-4.84 9.68-7.04 14.48l-.06.06c-.22.52-.48 1.06-.73 1.64-.16.35-.32.73-.48 1.15a6.8 6.8 0 007.2 9.23 8.38 8.38 0 003.18-1.1c1.3-.73 2.55-1.79 3.95-3.32 1.4 1.53 2.68 2.59 3.95 3.33A8.38 8.38 0 0022.75 32a6.79 6.79 0 006.75-5.83 5.94 5.94 0 00-.26-3.5zm-14.36 1.66c-1.72-2.2-2.84-4.22-3.22-5.95a5.2 5.2 0 01-.1-1.96c.07-.51.26-.96.52-1.34.6-.87 1.65-1.41 2.8-1.41a3.3 3.3 0 012.8 1.4c.26.4.45.84.51 1.35.1.58.06 1.25-.1 1.96-.38 1.7-1.5 3.74-3.21 5.95zm12.74 1.48a4.76 4.76 0 01-2.9 3.75c-.76.32-1.6.41-2.42.32-.8-.1-1.6-.36-2.42-.84a15.64 15.64 0 01-3.63-3.1c2.1-2.6 3.37-4.97 3.85-7.08.23-1 .26-1.9.16-2.73a5.53 5.53 0 00-.86-2.2 5.36 5.36 0 00-4.49-2.28c-1.85 0-3.5.86-4.5 2.27a5.18 5.18 0 00-.85 2.21c-.13.84-.1 1.77.16 2.73.48 2.11 1.78 4.51 3.85 7.1a14.33 14.33 0 01-3.63 3.12c-.83.48-1.62.73-2.42.83a4.76 4.76 0 01-5.32-4.07c-.1-.8-.03-1.6.29-2.5.1-.32.25-.64.41-1.02.22-.52.48-1.06.73-1.6l.04-.07c2.16-4.77 4.52-9.64 6.97-14.41l.1-.2c.25-.48.5-.99.76-1.47.26-.51.54-1 .9-1.4a3.32 3.32 0 015.09 0c.35.4.64.89.9 1.4.25.48.5 1 .76 1.47l.1.2c2.44 4.77 4.8 9.64 7 14.41l.03.03c.26.52.48 1.1.73 1.6.16.39.32.7.42 1.03.19.9.29 1.7.19 2.5z"></path></svg>
-          </a></div>
 
-          <div>
+            <h1 className="text-2xl pl-2">airbnb</h1>
+          </a>
+
+          </div>
+          <div className="flex justify-center mx-8">
+
             <div>
               <a href="#">Places to stay</a>
+              <div className="border-b-2 w-4 ml-12 pb-2"></div>
             </div>
+
+
             <div>
-              <a href="#">Experiences</a>
+              <a href="#" className="px-8">Experiences</a>
             </div>
+
+            <div>
+              <a href="#">online Experience</a>
+            </div>
+
           </div>
-          <div>
-            <a href="#">online Experience</a>
-          </div>
-          <div>
+          <div className="flex justify-center">
             <nav>
               <a href="#">Become a Host</a>
             </nav>
-            <div>
+            <div className="mx-8">
               <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" style={{ display: "block", height: "16px", width: "16px", fill: "currentcolor" }}><path d="m8.002.25a7.77 7.77 0 0 1 7.748 7.776 7.75 7.75 0 0 1 -7.521 7.72l-.246.004a7.75 7.75 0 0 1 -7.73-7.513l-.003-.245a7.75 7.75 0 0 1 7.752-7.742zm1.949 8.5h-3.903c.155 2.897 1.176 5.343 1.886 5.493l.068.007c.68-.002 1.72-2.365 1.932-5.23zm4.255 0h-2.752c-.091 1.96-.53 3.783-1.188 5.076a6.257 6.257 0 0 0 3.905-4.829zm-9.661 0h-2.75a6.257 6.257 0 0 0 3.934 5.075c-.615-1.208-1.036-2.875-1.162-4.686l-.022-.39zm1.188-6.576-.115.046a6.257 6.257 0 0 0 -3.823 5.03h2.75c.085-1.83.471-3.54 1.059-4.81zm2.262-.424c-.702.002-1.784 2.512-1.947 5.5h3.904c-.156-2.903-1.178-5.343-1.892-5.494l-.065-.007zm2.28.432.023.05c.643 1.288 1.069 3.084 1.157 5.018h2.748a6.275 6.275 0 0 0 -3.929-5.068z"></path></svg>
             </div>
-            <div>
-              <div>
-                <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" style={{ display: "block", fill: "none", height: "16px", width: "16px", stroke: "currentcolor", strokeWidth: "3", overflow: "visible" }}><g fill="none" fill-rule="nonzero"><path d="m2 16h28"></path><path d="m2 24h28"></path><path d="m2 8h28"></path></g>
+            <div className="bg-white rounded-full w-18 -mt-4 mr-8">
+
+              <div className="mx-8 -mt-2 mr-2 flex justify-between pt-4 pb-2 ml-2">
+                <svg viewBox="0 0 100 80" width="15" height="15" className="mr-2 mt-2">
+                  <rect width="100" height="20"></rect>
+                  <rect y="30" width="100" height="20"></rect>
+                  <rect y="60" width="100" height="20"></rect>
                 </svg>
-              </div>
-              <div>
-                <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" style={{ display: "block", height: "30px", width: "30px", fill: "currentcolor" }}><path d="m16 .7c-8.437 0-15.3 6.863-15.3 15.3s6.863 15.3 15.3 15.3 15.3-6.863 15.3-15.3-6.863-15.3-15.3-15.3zm0 28c-4.021 0-7.605-1.884-9.933-4.81a12.425 12.425 0 0 1 6.451-4.4 6.507 6.507 0 0 1 -3.018-5.49c0-3.584 2.916-6.5 6.5-6.5s6.5 2.916 6.5 6.5a6.513 6.513 0 0 1 -3.019 5.491 12.42 12.42 0 0 1 6.452 4.4c-2.328 2.925-5.912 4.809-9.933 4.809z"></path></svg>
+
+                <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" style={{ display: "block", height: "35px", width: "35px", fill: "" }}><path d="m16 .7c-8.437 0-15.3 6.863-15.3 15.3s6.863 15.3 15.3 15.3 15.3-6.863 15.3-15.3-6.863-15.3-15.3-15.3zm0 28c-4.021 0-7.605-1.884-9.933-4.81a12.425 12.425 0 0 1 6.451-4.4 6.507 6.507 0 0 1 -3.018-5.49c0-3.584 2.916-6.5 6.5-6.5s6.5 2.916 6.5 6.5a6.513 6.513 0 0 1 -3.019 5.491 12.42 12.42 0 0 1 6.452 4.4c-2.328 2.925-5.912 4.809-9.933 4.809z"></path></svg>
                 <div className="has-notification"></div>
               </div>
             </div>
@@ -41,45 +53,46 @@ function App() {
 
         </div>
 
-        <div>
+        <div className="ml-44 mb-8 bg-white mr-56 rounded-full h-16">
           <form action="#" method="">
-            <div>
+            <div className="flex justify-between">
 
-              <div>
+              <div className="pl-8">
                 <label>
                   <div>
-                    <div>Location</div>
+                    <div className="text-black pt-4">Location</div>
                     <input placeholder="Where are you going ?" />
                   </div>
                 </label>
               </div>
 
-              <div>
-                <div>
-                  <div>Check in</div>
-                  <div>Add dates</div>
-                </div>
-                <div>
-                  <div>Check out</div>
-                  <div>Add dates</div>
-                </div>
+
+              <div className="pt-4">
+                <div className="text-black">Check in</div>
+                <div className="text-gray-400">Add dates</div>
               </div>
 
-              <div>
-                <div>
-                  <div>Guests</div>
-                  <div>Add guests</div>
-                </div>
-                <div>
-                  <button>
-                    <div>
-                      <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" style={{ display: "block", fill: "none", height: "16px", width: "16px", stroke: "currentcolor", strokeWidth: "4", overflow: "visible" }}><g fill="none"><path d="m13 24c6.0751322 0 11-4.9248678 11-11 0-6.07513225-4.9248678-11-11-11-6.07513225 0-11 4.92486775-11 11 0 6.0751322 4.92486775 11 11 11zm8-3 9 9"></path></g></svg>
-                    </div>
-                    <div>Search</div>
-                  </button>
-                </div>
-
+              <div className="pt-4">
+                <div className="text-black">Check out</div>
+                <div className="text-gray-400">Add dates</div>
               </div>
+
+
+
+              <div className="pt-4">
+                <div className="text-black">Guests</div>
+                <div className="text-gray-400">Add guests</div>
+              </div>
+              <div className="pt-2 ml-8 mr-4">
+                <button className="bg-pink-800  rounded-full p-2">
+                  <div className="p-2">
+                    <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" style={{ display: "block", fill: "none", height: "20px", width: "20px", stroke: "currentcolor", strokeWidth: "4", overflow: "visible" }}><g fill="none"><path d="m13 24c6.0751322 0 11-4.9248678 11-11 0-6.07513225-4.9248678-11-11-11-6.07513225 0-11 4.92486775-11 11 0 6.0751322 4.92486775 11 11 11zm8-3 9 9"></path></g></svg>
+                  </div>
+                  <div></div>
+                </button>
+              </div>
+
+
             </div>
           </form>
         </div>
@@ -89,7 +102,7 @@ function App() {
         <div>
           <img src="https://a0.muscache.com/im/pictures/53e51dcb-8fad-4ce8-b61c-8a7a369267bf.jpg?im_w=1200" alt="Background image" />
           <div>
-            <h1>Not sure where to go?&nbsp;Perfect.</h1>
+            <h1 className="text-3xl">Not sure where to go?&nbsp;Perfect.</h1>
             <a>
               <span>I'm flexible</span>
             </a>
@@ -102,8 +115,9 @@ function App() {
             <div>
               <span>Introducing</span>
             </div>
-            <h2>
-              <span>Airbnb Gift Cards</span>
+            <h2 className="text-5xl">
+              <span>Airbnb</span>
+              <span> Gift Cards</span>
             </h2>
             <div>
               <a>
@@ -114,7 +128,7 @@ function App() {
         </div>
 
         <div>
-          <h2>Inspiration for your next&nbsp;trip</h2>
+          <h2 className="text-3xl">Inspiration for your next&nbsp;trip</h2>
           <div>
 
             <div>
@@ -123,7 +137,7 @@ function App() {
                   <img src="https://a0.muscache.com/im/pictures/ee86b8d6-b214-43f5-912c-3db2637a5724.jpg?im_w=480" alt="location" />
                 </div>
                 <div>
-                  <span>Abuja</span>
+                  <span className="text-3xl">Abuja</span>
                 </div>
                 <div>
                   <span>122 kilometers&nbsp;away</span>
@@ -137,7 +151,7 @@ function App() {
                   <img src="https://a0.muscache.com/im/pictures/ee86b8d6-b214-43f5-912c-3db2637a5724.jpg?im_w=480" alt="location" />
                 </div>
                 <div>
-                  <span>Enugu</span>
+                  <span className="text-3xl">Enugu</span>
                 </div>
                 <div>
                   <span>397 kilometers&nbsp;away</span>
@@ -151,7 +165,7 @@ function App() {
                   <img src="https://a0.muscache.com/im/pictures/ee86b8d6-b214-43f5-912c-3db2637a5724.jpg?im_w=480" alt="location" />
                 </div>
                 <div>
-                  <span>Asaba</span>
+                  <span className="text-3xl">Asaba</span>
                 </div>
                 <div>
                   <span>446 kilometers&nbsp;away</span>
@@ -165,7 +179,7 @@ function App() {
                   <img src="https://a0.muscache.com/im/pictures/ee86b8d6-b214-43f5-912c-3db2637a5724.jpg?im_w=480" alt="location" />
                 </div>
                 <div>
-                  <span>Kaduna</span>
+                  <span className="text-3xl">Kaduna</span>
                 </div>
                 <div>
                   <span>85 kilometers&nbsp;away</span>
@@ -459,8 +473,14 @@ function App() {
                       </li>
 
                     </ul>
+
+
                   </div>
+
+
+
                 </div>
+
               </section>
             </div>
           </footer>
